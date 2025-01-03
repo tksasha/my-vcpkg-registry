267b849b9e4e2564cdefb1776be7b85df92cfb65
Oleksandr Tkachuk <tksasha.me@gmail.com>
2025-01-03 11:20:13 +0200

    wip

--- ports/fmt/portfile.cmake
+++ ports/fmt/portfile.cmake
@@ -1,6 +1,6 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO git@github.com:tksasha/fmtlib/fmt
+    REPO git@github.com:tksasha/fmt
     REF "${VERSION}"
     SHA512 47ff6d289dcc22681eea6da465b0348172921e7cafff8fd57a1540d3232cc6b53250a4625c954ee0944c87963b17680ecbc3ea123e43c2c822efe0dc6fa6cef3
     HEAD_REF master
